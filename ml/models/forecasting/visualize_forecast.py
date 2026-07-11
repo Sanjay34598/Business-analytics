8ba4c2a1d61e6ec36426188a4438a7707b07c146
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-11 21:46:14 +0530

    feat: visualize forecasting results

---
+++ ml/models/forecasting/visualize_forecast.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+forecast =pd.read_csv("ml/data/processed/forecast_results.csv")
+
+plt.figure(figsize=(10,5))
+
+plt.plot(
+	forecast["Actual_Sales"].values[:50],
+	label = "Actual"
+)
+
+plt.plot(
+	forecast["Predicted_Sales"].values[:50],
+	label = "Predicted"
+)
+
+plt.title("Acutal vs Predicted Sales")
+plt.xlabel("Samples")
+plt.ylabel("Sales")
+
+plt.legend()
+plt.tight_layout()
+plt.show()
