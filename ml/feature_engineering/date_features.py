619112c6c7daa8932e83c5ceae16a64d46cfc63c
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-11 08:21:13 +0530

    feat: create date features

---
+++ ml/feature_engineering/date_features.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+sales = pd.read_csv("ml/data/cleaned/sales_data_cleaned.csv")
+
+sales["Sale_Date"] = pd.to_datetime(sales["Sale_Date"])
+
+sales["Year"] = sales["Sale_Date"].dt.year
+sales["Month"] = sales["Sale_Date"].dt.month
+sales["Month_Name"] = sales["Sale_Date"].dt.month_name()
+sales["Quarter"] = sales["Sale_Date"].dt.quarter
+sales["Day"] = sales["Sale_Date"].dt.day
+sales["Day_Name"] = sales["Sale_Date"].dt.day_name()
+sales["Weekday"]=sales["Sale_Date"].dt.weekday
+sales["Is_Weekend"] = sales["Weekday"].apply(lambda x: 1 if x >=5 else 0)
+
+print(sales.head())
