ab1990e90bab2df535127a8dffb172cb4370b11b
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 19:11:58 +0530

    update footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+
+        <footer
+            style={{
+                textAlign: "center",
+                padding: "20px",
+                marginTop: "40px",
+                color: "#666"
+            }}
+        >
+
+            © 2026 Business Analytics Dashboard
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
