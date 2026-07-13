15a71f11ef68acc6f53e90b196311e269ae93029
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 18:01:25 +0530

    updates the cofig.py

--- ml/pipeline/config.py
+++ ml/pipeline/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PROJECT_ROOT	= path(__file__).resolve().parents[2]
+PROJECT_ROOT	= Path(__file__).resolve().parents[2]
 
 RAW_DATA = PROJECT_ROOT / "ml/data/raw/sales_data.csv"
 CLEANED_DATA = PROJECT_ROOT / "ml/data/cleaned/sales_data_cleaned.csv"
