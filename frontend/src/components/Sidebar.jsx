a010f55d982e31e2d80df4a687bca5e2c42eecee
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 18:59:54 +0530

    modfied

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/Sidebar.css";
+
+function Sidebar() {
+
+    return (
+
+        <div className="sidebar">
+
+            <h2>Business Analytics</h2>
+
+            <ul>
+
+                <li>Dashboard</li>
+                <li>Sales</li>
+                <li>Forecast</li>
+                <li>Customers</li>
+                <li>Inventory</li>
+                <li>Reports</li>
+
+            </ul>
+
+        </div>
+
+    );
+
+}
+
+export default Sidebar;
