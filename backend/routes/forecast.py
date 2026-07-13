8d2b8913dbd5ccda4bbb27617c37044ff56f0a93
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 18:16:58 +0530

    new file: forecast .py

---
+++ backend/routes/forecast.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+from backend.services.load_data import forecast
+
+forecast_bp = Blueprint("forecast", __name__)
+
+@forecast_bp.route("/forecast")
+def get_forecast():
+
+    return jsonify(
+        forecast.to_dict(orient="records")
+    )
