a46359d90c29dbf13f383b255f93c0b38369d9bf
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 18:56:09 +0530

    new code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Sales from "./pages/Sales";
-import Customers from "./pages/Customers";
-import Inventory from "./pages/Inventory";
-import Forecast from "./pages/Forecast";
-import Reports from "./pages/Reports";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sales" element={<Sales />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/forecast" element={<Forecast />} />
-        <Route path="/reports" element={<Reports />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
+    return (
+
+        <Dashboard/>
+
+    );
+
 }
 
 export default App;
