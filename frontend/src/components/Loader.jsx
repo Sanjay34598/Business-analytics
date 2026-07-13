f2392173dbc691bc9703d09534d30ec6578f5ff4
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-13 19:10:47 +0530

    modified loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Loader() {
+
+    return (
+
+        <div
+            style={{
+                textAlign: "center",
+                padding: "40px",
+                fontSize: "20px"
+            }}
+        >
+
+            Loading...
+
+        </div>
+
+    );
+
+}
+
+export default Loader;
