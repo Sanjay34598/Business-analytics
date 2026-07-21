da59c65070ea4233d9d352971ae9e77fa53bb305
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-21 19:44:54 +0530

    recommendation

--- backend/routes/recommendation.py
+++ backend/routes/recommendation.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 import services.load_data as ld
 
-recommend_bp = Blueprint("recommend", __name__)
+recommendation_bp = Blueprint("recommendation", __name__)
 
-@recommend_bp.route("/recommendation")
+@recommendation_bp.route("/recommendation")
 def get_recommendation():
 
     return jsonify(
