b1fa7199acd0098fb3aef9dd68a130366c5eb638
Nalla Sanjay <sanjaynalla23890@gmail.com>
2026-07-21 17:00:20 +0530

    to load data

--- backend/services/load_data.py
+++ backend/services/load_data.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import os
 
-sales = pd.read_csv("ml/data/processed/sales_processed.csv")
-forecast = pd.read_csv("ml/data/processed/forecast_results.csv")
-churn = pd.read_csv("ml/data/processed/churn_prediction.csv")
-recommendation = pd.read_csv("ml/data/processed/product_recommend.csv")
-segments = pd.read_csv("ml/data/processed/sales_clustered.csv")
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+data_dir = os.path.join(base_dir, "ml", "data", "processed")
+
+sales = pd.read_csv(os.path.join(data_dir, "sales_processed.csv"))
+forecast = pd.read_csv(os.path.join(data_dir, "forecast_results.csv"))
+churn = pd.read_csv(os.path.join(data_dir, "churn_prediction.csv"))
+recommendation = pd.read_csv(os.path.join(data_dir, "product_recommend.csv"))
+segments = pd.read_csv(os.path.join(data_dir, "sales_clustered.csv"))
 
